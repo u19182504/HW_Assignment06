5c605ad62515a2983955ca282623a604750cf909
u19182504@tuks.co.za <u19182504@tuks.co.za>
2022-10-30 23:52:42 +0200

    Changed order controller

--- HWAssignment06/Controllers/OrdersController.cs
+++ HWAssignment06/Controllers/OrdersController.cs
@@ -86,47 +86,6 @@ namespace HWAssignment06.Controllers
         }
 
         // GET: Orders/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Orders/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Orders/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Orders/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
